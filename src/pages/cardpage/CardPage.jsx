653920760bb113982eb794ab644803cff7f07737
rakshit-singh1 <thisisjames007@gmail.com>
2025-01-20 14:31:35 +0530

    card pade

--- src/pages/cardpage/CardPage.jsx
+++ src/pages/cardpage/CardPage.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { readContract, waitForTransactionReceipt, writeContract } from 'wagmi/actions';
+import { readContract } from 'wagmi/actions';
 import abi from "../../helper/ManagerFaucetAbi.json";
-import TokenABi from "../../helper/TokenAbi.json";
 import DegenFacetabi from "../../helper/DegenFacetAbi.json";
 import { daimond } from '../../helper/Helper';
+import TokenAbi from '../../helper/TokenAbi.json';
 import { useAccount, useReadContract } from 'wagmi';
 import logo from "../../assets/logo/logo.png";
 import TradeEventList from '../../components/Statistics/TradeEventList';
 import { config } from '../../wagmiClient';
-import { formatUnits } from 'ethers';
 import { Line } from 'react-chartjs-2';
 import { Chart, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale } from 'chart.js';
+import BuySell from '../../components/BuySell/BuySell';
+import { useEffect } from 'react';
 const CardPage = () => {
   const { token } = useParams();
 
@@ -27,44 +28,14 @@ const CardPage = () => {
     chainId: 97,
   });
 
-  const { chain } = useAccount();
+  const { chain, address } = useAccount();
 
-  const [amount, setAmount] = useState(0);
-  const [timeLeft, setTimeLeft] = useState(0);
-  const [isSaleAvailable, setIsSaleAvailable] = useState(false);
-  const [approve, setApprove] = useState(0);
   const [amountOut, setAmountOut] = useState([0n, 0n, 0n, 0n, 0n]);
   const [isBuy, setIsBuy] = useState(true);
   const [txDone, setTxDone] = useState(0);
+  const [balanceOf, setBalaceOf] = useState(0);
 
   const startTime = data?.startTime ? Math.floor(Number(data.startTime) / 1000) : 0;
-
-  // ** Hook 1: Calculate timeLeft for sale countdown **
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const currentTime = Math.floor(new Date().getTime() / 1000);
-      const difference = startTime - currentTime;
-
-      if (difference <= 0) {
-        setIsSaleAvailable(true);
-        clearInterval(interval);
-      } else {
-        const days = Math.floor(difference / (24 * 3600));
-        const hours = Math.floor((difference % (24 * 3600)) / 3600);
-        const minutes = Math.floor((difference % 3600) / 60);
-        const seconds = Math.floor(difference % 60);
-        setTimeLeft(`${days}d ${hours}h ${minutes}m ${seconds}s`);
-      }
-    }, 1000); 
-
-    return () => clearInterval(interval);
-  }, [startTime]);
-
-  // ** Hook 2: Fetch amountOut value based on amount and isBuy **
-  useEffect(() => {
-    if (amount <= 0 || !token) return; // Prevent fetching if amount is invalid
-    fetchAmountOut(amount);
-  }, [amount, isBuy, token]);
 
   const fetchAmountOut = async (inputAmount) => {
     try {
@@ -79,97 +50,34 @@ const CardPage = () => {
           isBuy,
         ],
       });
-      console.log({result})
-      setAmountOut(result);
     } catch (error) {
       console.error('Error fetching amountOut:', error);
       setAmountOut([0n, 0n, 0n, 0n, 0n]);
     }
   };
 
-  const handleBuy = async () => {
-    if (!token || !amount) return;
-
+  const fetchBalaceOf = async () => {
     try {
-      const data = await writeContract(config, {
-        address: daimond,
-        abi: DegenFacetabi,
-        functionName: 'buy',
-        chainId: 97,
-        args: [
-          token,
-          "0x0000000000000000000000000000000000000000",
-          BigInt(amount * 10 ** 18),
-          amountOut[0],
-          [],
-        ],
-        // value: BigInt(amount * 10 ** 18),
-        value: BigInt(amount * 10 ** 18) + amountOut[2],
-      });
-      const receipt = await waitForTransactionReceipt(config, {
-        hash: data,
-      })
-      setTxDone((prev) => prev + 1);
-    } catch (error) {
-      console.error('Error during buy:', error);
-    }
-  };
-
-  const handleApprove = async () => {
-    if (!token || !amount) return;
-
-    try {
-      const data = await writeContract(config, {
+      const result = await readContract(config, {
+        abi: TokenAbi,
         address: token,
-        abi: TokenABi,
-        functionName: 'approve',
+        functionName: 'balanceOf',
         chainId: 97,
         args: [
-          daimond,
-          BigInt(amount * 10 ** 18),
+          address,
         ],
       });
-      const receipt = await waitForTransactionReceipt(config, {
-        hash: data,
-      })
-      setApprove(amount);
+      setBalaceOf(result)
     } catch (error) {
-      console.log('Error during approve:', error);
+      console.error('Error fetching amountOut:', error);
+      setAmountOut([0n, 0n, 0n, 0n, 0n]);
     }
   };
 
-  const handleSell = async () => {
-    if (!token || !approve) return;
 
-    console.log([
-      token,
-      "0x0000000000000000000000000000000000000000",
-      BigInt(approve * 10 ** 18),
-      amountOut[0],
-    ])
-    try {
-      const data = await writeContract(config, {
-        address: daimond,
-        abi: DegenFacetabi,
-        functionName: 'sell',
-        chainId: 97,
-        args: [
-          token,
-          "0x0000000000000000000000000000000000000000",
-          BigInt(approve * 10 ** 18),
-          amountOut[0]-amountOut[4],
-        ],
-      });
-      
-      const receipt = await waitForTransactionReceipt(config, {
-        hash: data,
-      })
-      setApprove(0);
-      setTxDone((prev) => prev + 1);
-    } catch (error) {
-      console.error('Error during sell:', error);
-    }
-  };
+  useEffect(() => {
+    fetchBalaceOf();
+  }, [address]);
 
   if (isLoading) {
     return (
@@ -253,6 +161,14 @@ const CardPage = () => {
     },
   };
 
+  let routerText = '';
+  if (data?.router === '0xD99D1c33F9fC3444f8101754aBC46c52416550D1') {
+    routerText = 'Pancake Swap';
+  } else if (data?.router === '0xda8e9632c013c9d6a5fbabac9e2ecdf69706a306') {
+    routerText = 'How Swap';
+  }
+
+  console.log({data})
 
   return (
     <div className="container px-4 px-lg-5 mx-auto">
@@ -272,6 +188,7 @@ const CardPage = () => {
             <li><strong>Symbol:</strong> {poolDetailsParsed.symbol}</li>
             <li><strong>Description:</strong> {poolDetailsParsed.description}</li>
             <li><strong>Tag:</strong> {poolDetailsParsed.Tag}</li>
+            <li><strong>Router:</strong> {routerText}</li>
           </ul>
         </div>
 
@@ -291,117 +208,49 @@ const CardPage = () => {
                 </a>
               </strong>
             </li>
+            <li>
+              <strong>Website :
+                <a
+                  href={poolDetailsParsed.Website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="ml-3 text-gray-500 hover:underline hover:text-gold"
+                >
+                  {poolDetailsParsed.Website}
+                </a>
+              </strong>
+            </li>
+            <li>
+              <strong>Twitter :
+                <a
+                  href={poolDetailsParsed.Twitter}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="ml-3 text-gray-500 hover:underline hover:text-gold"
+                >
+                  {poolDetailsParsed.Twitter}
+                </a>
+              </strong>
+            </li>
+            <li>
+              <strong>Telegram :
+                <a
+                  href={poolDetailsParsed.Telegram}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="ml-3 text-gray-500 hover:underline hover:text-gold"
+                >
+                  {poolDetailsParsed.Telegram}
+                </a>
+              </strong>
+            </li>
             <li><strong>Start Time:</strong> {data?.startTime ? new Date(Number(data.startTime) * 1000).toLocaleString() : 'N/A'}</li>
           </ul>
         </div>
 
         {/* Buy/Sell Section or Countdown */}
         <div className="boxc bg-white p-6 rounded-lg">
-          <div className="mt-4 buysell">
-            {isSaleAvailable ? (
-              <>
-                <div className="btngroup flex mb-6">
-                  <button
-                    className={`buy px-6 py-3 text-white text-md font-semibold rounded-lg transition duration-300 w-full ${!isBuy ? '' : 'bg-gold'}`}
-                    onClick={() => setIsBuy(true)}>
-                    <h2>Buy Token</h2>
-                  </button>
-                  <button
-                    className={`sell px-6 py-3 text-white text-md font-semibold rounded-lg  transition duration-300 w-full ${isBuy ? '' : 'bg-gold'}`}
-                    onClick={() => setIsBuy(false)}>
-                    <h2>Sell Token</h2>
-                  </button>
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-600">
-                    Amount of {isBuy ? (chain?.nativeCurrency?.symbol || "Currency") : poolDetailsParsed.symbol}
-                  </label>
-
-                  <input
-                    type="number"
-                    className="mt-2 w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    placeholder={`Enter amount in ${poolDetailsParsed.symbol}`}
-                    value={amount}
-                    onChange={(e) => setAmount(Number(e.target.value))}
-                    min="0"
-                  />
-                </div>
-
-                <div className="text-black">
-                  {isBuy ? (
-                    <p>Buy Tax: <span className='receivedvalu'>{parseInt(data?.buyFeeRate)}%</span></p>
-                  ) : (
-                    <p>Sell Tax: <span className='receivedvalu'>{parseInt(data?.sellFeeRate)}%</span></p>
-                  )}
-                  <p> Received : <span className='receivedvalu'>{formatUnits(amountOut[0], 18)} </span></p>
-                  <p> Min Received : <span className='receivedvalu'>
-                    {isBuy ?
-                      formatUnits(amountOut[1], 18) * (1 - parseInt(data?.buyFeeRate) / 100) :
-                      formatUnits(amountOut[1], 18) * (1 - parseInt(data?.sellFeeRate) / 100)
-                    }
-                  </span></p>
-                </div>
-                <div className="flex justify-between space-x-4">
-                  {isBuy ? (
-                    <button
-                      className="lasstbutton w-full bg-gold text-white py-3 rounded-lg hover:bg-gold transition duration-300"
-                      onClick={handleBuy}
-                    >
-                      Buy Token
-                    </button>
-                  ) : (
-                    <>
-                      {approve === 0 && (
-                        <button
-                          className="lasstbutton w-full bg-gray-500 text-white py-3 rounded-lg transition duration-300"
-                          onClick={handleApprove}
-                        >
-                          Approve
-                        </button>
-                      )}
-                      {approve > 0 && (
-                        <button
-                          className="w-full bg-red-600 text-white py-3 rounded-lg hover:bg-red-700 transition duration-300"
-                          onClick={handleSell}
-                        >
-                          Sell {approve}
-                        </button>
-                      )}
-                    </>
-                  )}
-                </div>
-              </>
-            ) : (
-              <div className="flex flex-col items-center justify-center bg-gold from-blue-500 to-purple-600 p-6 rounded-lg shadow-lg w-full max-w-xs mx-auto">
-                <h2 className="text-2xl font-semibold text-white mb-4">Sale Starts In</h2>
-                <div className="flex space-x-4 mb-4">
-                  {timeLeft && timeLeft.split(' ').length === 4 && (
-                    <>
-                      <div className="text-center text-white">
-                        <p className="text-lg font-bold">{timeLeft.split(' ')[0]}</p>
-                        <span className="text-sm">Days</span>
-                      </div>
-                      <div className="text-center text-white">
-                        <p className="text-lg font-bold">{timeLeft.split(' ')[1]}</p>
-                        <span className="text-sm">Hours</span>
-                      </div>
-                      <div className="text-center text-white">
-                        <p className="text-lg font-bold">{timeLeft.split(' ')[2]}</p>
-                        <span className="text-sm">Minutes</span>
-                      </div>
-                      <div className="text-center text-white">
-                        <p className="text-lg font-bold">{timeLeft.split(' ')[3]}</p>
-                        <span className="text-sm">Seconds</span>
-                      </div>
-                    </>
-                  )}
-                </div>
-                <div className="w-full bg-white rounded-lg py-3 px-6 text-center text-black">
-                  <p className="text-lg font-semibold">Sale will begin soon!</p>
-                </div>
-              </div>
-            )}
-          </div>
+          <BuySell data={data} token={token} balanceOf={balanceOf}/>
         </div>
 
       </div>
