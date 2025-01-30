d3085779e85fbf6ed271fc6f57a5e1be6398cd6d
rakshit-singh1 <thisisjames007@gmail.com>
2025-01-30 14:54:40 +0530

    added chain 56

--- src/helper/Helper.js
+++ src/helper/Helper.js
@@ -7,16 +7,12 @@ export const tags = ['Meme', 'AI', 'DeFi', 'Games', 'Infra', 'De-Sci', 'Social',
 // Routers
 
 const bscRouters = {
-    'UniswapV2': '0x4Dae2f939ACf50408e13d58534Ff8c2776d45265',
-    'Pancake Swap': '0x10ED43C718714eb63d5aA57B78B54704E256024E',
-    'PipiSwap': '0x300eb5d633c8154466afc73e98328e882e3d843d',
-    'BabyDogeSwap': '0xC9a0F685F39d05D835c369036251ee3aEaaF3c47',
-    'MelegaSwap': '0xc25033218D181b27D4a2944Fbb04FC055da4EAB3',
+    'Howswap': '0x84dfe8da2c41e8d536748bc0195d7fe6a39d3fae',
 }
 
 const bscTestnetRouters = {
     'Pancake Swap': '0xD99D1c33F9fC3444f8101754aBC46c52416550D1',
-    'House Swap': '0xda8e9632c013c9d6a5fbabac9e2ecdf69706a306', // test
+    'HowSwap': '0xda8e9632c013c9d6a5fbabac9e2ecdf69706a306', // test
 }
 
 export const routers = {
